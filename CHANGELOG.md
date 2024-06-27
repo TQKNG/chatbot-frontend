2f867a7639f73b2169d0a7d534961fbd19c1147f
Khanh Ng <83807651+TQKNG@users.noreply.github.com>
2024-06-27 10:12:23 -0400

    v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,13 +14,14 @@
 - Bug 1
 - Bug 2
 
-## [0.1.0] - 2024-06-05
-
-### Added
-- Initial release
-
 
 ## [1.0.0] - 2024-06-05
 
 ### Added
 - Version 1 release
+
+## [1.0.1] - 2024-06-27
+
+### Added
+- Change UI design
+- Update YAML file
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /*@type {import('next').NextConfig}*/
-const nextConfig = {
-  reactStrictMode: true,
-  distDir: 'build',
-  output: 'standalone',
-};
+// const nextConfig = {
+//   reactStrictMode: true,
+//   distDir: 'build',
+//   output: 'standalone',
+// };
 
-export default nextConfig;
+// export default nextConfig;
