f257eb82c29720900942c79efb62ea542b5354cf
Tran Quoc Khanh Nguyen <khanhnguyen.trq@gmail.com>
2024-07-24 01:39:29 -0400

    test stream 1st pass

--- app/api/aichatbot/route.ts
+++ app/api/aichatbot/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 export const dynamic= 'force-dynamic'
+
+
 export async function POST(req:Request, res:Response){
     const body  = await req.json();
     let response = null;
@@ -32,18 +34,48 @@ export async function POST(req:Request, res:Response){
     // console.log("test response", data)
 
 
-    response.body?.pipeTo(new WritableStream({
-        write(chunk){
-            console.log(chunk)
-            Response.json({data:chunk})
+    // test readable stream
+    const stream = new ReadableStream({
+        async start(controller){
+            const encoder = new TextEncoder();
+            // controller.enqueue(encoder.encode('data: start agent\n\n'));
+            controller.enqueue(encoder.encode(`data: ${await response.text()}\n\n`));
 
-        },
-        close(){
-            console.log("Stream closed")
-        },
-        abort(){
-            console.log("Stream aborted")
+            // Simulate streaming data
+            setTimeout(() => {
+                controller.enqueue(encoder.encode(`data: ${response.text()}\n\n`));
+                controller.close();
+            }, 2000);
         }
-    }))
+    })
+
+    return new Response(stream, {
+        headers: {
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive',
+        },
+    })
+
+
+
+    // const stream = response.body?.pipeTo(new WritableStream({
+    //     write(chunk){
+    //         const reader = new TextDecoder('utf-8')
+    //         const data = reader.decode(chunk)
+    //         let results;
+    //         if (data.includes("sql-db-query-checker")) {
+    //             results = 'initialize'
+    //         }
+    //         else results = data
+
+    //     },
+    //     close(){
+    //         console.log("Stream closed")
+    //     },
+    //     abort(){
+    //         console.log("Stream aborted")
+    //     }
+    // }))
     
 }
