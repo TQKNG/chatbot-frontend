fff1a82844fa947794c2038a40923b46f1c07602
Khanh Ng <83807651+TQKNG@users.noreply.github.com>
2024-07-31 16:38:30 -0400

    test voice

--- app/page.tsx
+++ app/page.tsx
@@ -254,7 +254,7 @@ export default function Home() {
           }
 
           // Restart the process
-          startListening();
+          // startListening();
         };
 
         const checkSilence = () => {
