003b51fbb3a527769802f524b573e6228a4f944a
Khanh Ng <83807651+TQKNG@users.noreply.github.com>
2024-10-25 15:01:46 -0400

    Add knowledge base logo

--- app/page.tsx
+++ app/page.tsx
@@ -104,9 +104,9 @@ export default function Home() {
       }
 
       formData.append("question", value);
-      if(fileInputRef.current){
-        setKnowledgeBase(null)
-        fileInputRef.current.value = ""
+      if (fileInputRef.current) {
+        setKnowledgeBase(null);
+        fileInputRef.current.value = "";
       }
 
       const response = await fetch("/api/aichatbot", {
@@ -116,8 +116,6 @@ export default function Home() {
         // },
         body: formData,
       });
-
-   
 
       if (!response.ok) {
         throw new Error(response.statusText);
@@ -186,9 +184,9 @@ export default function Home() {
     }
 
     formData.append("question", value);
-    if(fileInputRef.current){
-      setKnowledgeBase(null)
-      fileInputRef.current.value = ""
+    if (fileInputRef.current) {
+      setKnowledgeBase(null);
+      fileInputRef.current.value = "";
     }
 
     const response = await fetch("/api/aichatbot", {
@@ -496,7 +494,7 @@ export default function Home() {
                   width="24px"
                   fill="#0098C1"
                 >
-                  <path d="M240-520h60v-80h-60v80Zm100 80h60v-240h-60v240Zm110 80h60v-400h-60v400Zm110-80h60v-240h-60v240Zm100-80h60v-80h-60v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z" />
+                  <path d="M240-80v-172q-57-52-88.5-121.5T120-520q0-150 105-255t255-105q125 0 221.5 73.5T827-615l52 205q5 19-7 34.5T840-360h-80v120q0 33-23.5 56.5T680-160h-80v80h-80v-160h160v-200h108l-38-155q-23-91-98-148t-172-57q-116 0-198 81t-82 197q0 60 24.5 114t69.5 96l26 24v208h-80Zm254-360Zm-54 80h80l6-50q8-3 14.5-7t11.5-9l46 20 40-68-40-30q2-8 2-16t-2-16l40-30-40-68-46 20q-5-5-11.5-9t-14.5-7l-6-50h-80l-6 50q-8 3-14.5 7t-11.5 9l-46-20-40 68 40 30q-2 8-2 16t2 16l-40 30 40 68 46-20q5 5 11.5 9t14.5 7l6 50Zm40-100q-25 0-42.5-17.5T420-520q0-25 17.5-42.5T480-580q25 0 42.5 17.5T540-520q0 25-17.5 42.5T480-460Z" />
                 </svg>
                 <div className="form-control">
                   <label className="flex justify-between gap-4 cursor-pointer">
@@ -565,13 +563,13 @@ export default function Home() {
 
               {/* Section: Chat */}
               <div className="w-full flex flex-col items-center justify-center text-white">
-                 <span>{knowledgeBase?knowledgeBase?.name:null}</span>
+                <span>{knowledgeBase ? knowledgeBase?.name : null}</span>
                 {/* User input */}
                 <div className="relative flex justify-center items-center w-full max-w-4xl">
                   {/* Attachment doc */}
                   <div
                     className="flex items-center absolute cursor-pointer left-1"
-                    onClick={()=>{
+                    onClick={() => {
                       fileInputRef.current?.click();
                     }}
                   >
@@ -610,7 +608,10 @@ export default function Home() {
                     onClick={handleSend}
                   />
                 </div>
-                <FileUpload onFileUpload={handleFileUpload} fileInputRef={fileInputRef}  />
+                <FileUpload
+                  onFileUpload={handleFileUpload}
+                  fileInputRef={fileInputRef}
+                />
               </div>
             </div>
           </>
